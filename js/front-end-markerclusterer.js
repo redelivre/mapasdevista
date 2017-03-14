40dd36b29085d5b4d5b5209813965e499cccd2fd
Jacson Passold <jacson@ethymos.com.br>
2017-03-14 10:45:58 -0300

    fix cluster image url

--- js/front-end-markerclusterer.js
+++ js/front-end-markerclusterer.js
@@ -2,7 +2,7 @@ var mc; //global for debuging
 
 jQuery(document).ready(function() {
 
-	mc = new MarkerClusterer(mapstraction.getMap());
+	mc = new MarkerClusterer(mapstraction.getMap(), [], {imagePath : "/wp-content/plugins/mapasdevista/js/markerclustererplus/images/m"});
 	mc.setIgnoreHidden(true);
 	mc.setMaxZoom(15); // TODO create a option
 	
